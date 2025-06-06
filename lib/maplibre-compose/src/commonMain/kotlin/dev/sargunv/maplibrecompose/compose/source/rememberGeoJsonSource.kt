158516db8d0013ed60482c379fac03c41c1654fe
michalgwo <michalgw98@gmail.com>
2025-06-06 17:31:42 +0200

    added doc

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/compose/source/rememberGeoJsonSource.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/compose/source/rememberGeoJsonSource.kt
@@ -44,6 +44,11 @@ public fun rememberGeoJsonSource(
     )
   }
 
+/**
+ * Remember a new [GeoJsonSource] with the given [id] and [options] from the given serialized GeoJson [data].
+ *
+ * @throws IllegalArgumentException if a layer with the given [id] already exists.
+ */
 @Composable
 public fun rememberGeoJsonSource(
   id: String,
